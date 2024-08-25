c53dadb3445bdfe3bc106a7015adaada53343503
AllenLiao <allen10153@gmail.com>
2024-08-25 18:39:21 +0800

    feat: implement admin categories page and create category

---
+++ controllers/category-controller.js
@@ -0,0 +1,20 @@
+const { Category } = require('../models')
+
+const categoryController = {
+  getCategories: (req, res, next) => {
+    Category.findAll({ raw: true })
+      .then(categories => res.render('admin/categories', { categories }))
+      .catch(err => next(err))
+  },
+  postCategory: (req, res, next) => {
+    const { name } = req.body
+    if (!name) throw new Error('category name is required')
+    return Category.create({ name })
+      .then(() => {
+        req.flash('success_messages', 'category was successfully created')
+        res.redirect('/admin/categories')
+      })
+      .catch(err => next(err))
+  }
+}
+module.exports = categoryController
